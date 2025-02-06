83ec6f7a756454a87da5792bcf9b70724d49e65a
Jesús Pérez <jesusp345@gmail.com>
2025-02-06 13:31:28 +0100

    Primer commit

---
+++ script.js
@@ -0,0 +1,24 @@
+/*alert("Hola, " + nombre + "!");
+let color = prompt("Escribe tu color favorito");
+
+const elemento = document.getElementById("titulo");
+
+elemento.style.background = color;
+elemento.innerText = "Hola, " + nombre + "!";*/
+
+const contrasenya = document.getElementById("contraseña");
+let longitud
+
+
+function Contraseña (); { 
+if (longitud<=8)
+    elemento.innerText = "La contraseña es demasiado corta"
+} else if {
+    (longitud > 8 = 10)
+    elemento.innerText = "La contraseña no es del todo segura"
+
+} else {
+        (longitud <= 10)
+        elemento.innerText = "La contraseña es segura"
+
+    }
